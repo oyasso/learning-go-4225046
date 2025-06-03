7fb712355bf1060a154b43059a3e06398752a68f
oyasso <129062875+oyasso@users.noreply.github.com>
2025-06-03 20:26:17 +0000

    conditional logic

--- Ch04/04_01b/main.go
+++ Ch04/04_01b/main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+	// theAnswer := -5
+	var result string
+
+	if theAnswer := 42; theAnswer < 0 {
+		result = "Less than zero"
+	} else if theAnswer == 0 {
+		result = "Equal to zero"
+	} else {
+		result = "Greater than zero"
+	}
+	fmt.Println(result)
+	// fmt.Printf("Value of theAnswer: %v\n", theAnswer)
 }
