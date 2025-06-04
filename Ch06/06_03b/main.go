6b59f3995b6b212859f568da0a7c6b94bf97b003
oyasso <129062875+oyasso@users.noreply.github.com>
2025-06-04 22:31:46 +0000

    parse json-text

--- Ch06/06_03b/main.go
+++ Ch06/06_03b/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
 	content := readHttpContent()
-	fmt.Print(content)
+	// fmt.Print(content)
+	tours := toursFromJSON(content)
+	for _, tour := range tours {
+		price, _ := strconv.ParseFloat(tour.Price, 16)
+		fmt.Printf("%v: ($%v)\n", tour.Name, price)
+	}
+}
+
+func toursFromJSON(content string) []Tour {
+
+	tours := make([]Tour, 0)
+	decoder := json.NewDecoder(strings.NewReader(content))
+	_, err := decoder.Token()
+	checkError(err)
+
+	var tour Tour
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		checkError(err)
+		tours = append(tours, tour)
+	}
+	return tours
 }
 
 func readHttpContent() string {
@@ -37,4 +61,8 @@ func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+type Tour struct {
+	Name, Price string
 }
