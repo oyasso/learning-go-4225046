fb0b3c10b7d6b13ddf63b064156749fae597a9a1
oyasso <129062875+oyasso@users.noreply.github.com>
2025-06-04 20:48:51 +0000

    custom type functions

--- Ch05/05_02b/main.go
+++ Ch05/05_02b/main.go
@@ -6,10 +6,22 @@ import (
 
 func main() {
 	dog := Dog{"Poodle", "Woof"}
-	fmt.Printf("The %v says %v!\n", dog.Breed, dog.Sound)
+	dog.Speak()
+	dog.Sound = "Arf"
+	dog.Speak()
+
+	println(dog.SpeakThreeTimes())
 }
 
 type Dog struct {
-   Breed string
-   Sound string
+	Breed string
+	Sound string
+}
+
+func (d Dog) Speak() {
+	fmt.Printf("The %v says %v!\n", d.Breed, d.Sound)
+}
+
+func (d Dog) SpeakThreeTimes() string {
+	return fmt.Sprintf("%v! %v! %v!", d.Sound, d.Sound, d.Sound)
 }
