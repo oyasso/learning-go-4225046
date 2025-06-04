a61e5d2045bac4b548bf9ff112b6a365cb8c8670
oyasso <129062875+oyasso@users.noreply.github.com>
2025-06-04 20:39:53 +0000

    functions

--- Ch05/05_01b/main.go
+++ Ch05/05_01b/main.go
@@ -5,5 +5,31 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	doSomething()
+	value1 := 5
+	value2 := 10
+	value3 := 65
+	sum, count, average := addAllValues(value1, value2, value3)
+	fmt.Printf("The sum is %v\n", sum)
+	fmt.Printf("The count is %v\n", count)
+	fmt.Printf("The average is %v\n", average)
+
+}
+
+func doSomething() {
+	fmt.Println("Doing something")
+}
+
+func addValues(value1, value2 int) int {
+	return value1 + value2
+}
+
+func addAllValues(values ...int) (int, int, float64) {
+	sum := 0
+	for _, v := range values {
+		sum += v
+	}			
+	count := len(values)
+	average := float64(sum) / float64(count)	
+	return sum, count, average
 }
